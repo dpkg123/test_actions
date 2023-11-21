4c15f17137320135ea7eab31bd8c496d22ae54ed
טפל בלוט <130299352+dpkg123@users.noreply.github.com>
2023-11-21 20:56:32 +0800

    Update and rename build to build.py

---
+++ build.py
@@ -0,0 +1,6 @@
+import os
+import sys
+
+os.system("apt-get -y install git live-build debian-archive-keyring gpg dosfstools genisoimage squashfs-tools xorriso grub-common grub-pc-bin grub-efi-amd64-bin nano")
+os.system("lb config")
+os.system("lb build")
