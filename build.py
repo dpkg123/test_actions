eab7da0c31af3e8b8970370e17ae4d8e79c305ed
טפל בלוט <130299352+dpkg123@users.noreply.github.com>
2023-11-21 21:02:36 +0800

    Update build.py

--- build.py
+++ build.py
@@ -1,7 +1,5 @@
 import os
 import sys
 
-os.system("cd /root")
 os.system("apt-get -y install git live-build debian-archive-keyring gpg dosfstools genisoimage squashfs-tools xorriso grub-common grub-pc-bin grub-efi-amd64-bin nano")
-os.system("lb config")
-os.system("lb build")
+os.system("mkdir -p -v /ci && cd /ci && lb config && lb build")
