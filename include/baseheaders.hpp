dc136576e4343a477905a14ad41c837b26d6a1aa
dpkg123 <test1@qw5d.onmicrosoft.com>
2024-08-04 12:03:42 +0800

    2024年 08月 04日 星期日 12:03:41 CST

---
+++ include/baseheaders.hpp
@@ -0,0 +1,17 @@
+#ifndef BASEHEADERS_HPP
+#define BASEHEADERS_HPP
+
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+#include <ctime>
+#include <string>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <unistd.h>
+
+using namespace std;
+
+#endif
