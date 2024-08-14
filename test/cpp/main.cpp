4c4aa02460cd06c745413cc7eb1f7c2413b75ab1
dpkg123 <test1@qw5d.onmicrosoft.com>
2024-08-14 10:40:02 +0800

    2024年 08月 14日 星期三 10:40:01 CST

--- test/cpp/main.cpp
+++ test/cpp/main.cpp
@@ -5,6 +5,17 @@
 #include <stdbool.h>
 #include <sys/prctl.h>
 
+#if defined(__GNUC__) && __GNUC__ == 12
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wmaybe-uninitialized"  // Workaround for GCC 12
+#endif
+
+#include <catch2/catch_all.hpp>
+
+#if defined(__GNUC__) && __GNUC__ == 12
+    #pragma GCC diagnostic pop
+#endif
+
 using namespace std;
 
 #define CMD_GRANT_ROOT 0
