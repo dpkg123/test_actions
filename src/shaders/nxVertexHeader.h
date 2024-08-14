001bcdaef2fe494151f53d8396030d1d743f9642
dpkg123 <test1@qw5d.onmicrosoft.com>
2024-08-14 16:13:33 +0800

    2024年 08月 14日 星期三 16:13:32 CST

--- src/shaders/nxVertexHeader.h
+++ src/shaders/nxVertexHeader.h
@@ -1,6 +1,13 @@
 #version 430
 
-#include "nvn.h"
+#define MCPE_PLATFORM_NX
+
+//#if __VERSION__ >= 300
+#define MAT4 highp mat4
+#define POS4 highp vec4
+#define POS3 highp vec3
+precision highp float;
+//#endif
 
 out gl_PerVertex
 {
--- src/shaders/uniformPerFrameConstants.h
+++ src/shaders/uniformPerFrameConstants.h
@@ -1,14 +1,43 @@
 #ifndef _UNIFORM_PER_FRAME_CONSTANTS_H
 #define _UNIFORM_PER_FRAME_CONSTANTS_H
 
-#include "uniformMacro.h"
+#ifndef _UNIFORM_MACRO_H
+#define _UNIFORM_MACRO_H
+
+#ifdef MCPE_PLATFORM_NX
+// Unfortunately this macro does not work on old Amazon platforms #define BEGIN_UNIFORM_BLOCK(x) uniform x {
+#define END_UNIFORM_BLOCK };
+#define UNIFORM 
+#else
+// Unfortunately this macro does not work on old Amazon platforms #define BEGIN_UNIFORM_BLOCK(x) 
+#define END_UNIFORM_BLOCK
+#define UNIFORM uniform 
+#endif
+
+#if __VERSION__ >= 420
+#define LAYOUT_BINDING(x) layout(binding = x)
+#else
+#define LAYOUT_BINDING(x) 
+#endif
+
+#endif
+
 
 #ifdef MCPE_PLATFORM_NX
 layout(binding = 2) uniform PerFrameConstants {
 #endif
 // BEGIN_UNIFORM_BLOCK(PerFrameConstants) - unfortunately this macro does not work on old Amazon platforms so using above 3 lines instead
 UNIFORM vec3 VIEW_POS;
+#ifdef MCPE_NETEASE
+// TIME will loop from [0, 210]
+// make sure your shader handles the case when it transitions from 210 to 0
+#endif
 UNIFORM float TIME;
+#ifdef MCPE_NETEASE
+UNIFORM vec4 BLEND_COLOR;
+UNIFORM vec4 USER_FOR_COLOR_NEAR;
+UNIFORM vec4 USER_FOR_COLOR_FAR;
+#endif
 UNIFORM vec4 FOG_COLOR;
 UNIFORM vec2 FOG_CONTROL;
 UNIFORM float RENDER_DISTANCE;
