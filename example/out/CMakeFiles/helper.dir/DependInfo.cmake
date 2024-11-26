9669ffbd16f503fd5984ed5bd0691e7f772c95bf
dpkg123 <test1@qw5d.onmicrosoft.com>
2024-11-26 20:59:53 +0800

    2024年 11月 26日 星期二 20:59:53 CST

---
+++ example/out/CMakeFiles/helper.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/user/985211helper-liaoning2025/cpp/src/data.cpp" "CMakeFiles/helper.dir/src/data.cpp.o" "gcc" "CMakeFiles/helper.dir/src/data.cpp.o.d"
+  "/home/user/985211helper-liaoning2025/cpp/src/helper.cpp" "CMakeFiles/helper.dir/src/helper.cpp.o" "gcc" "CMakeFiles/helper.dir/src/helper.cpp.o.d"
+  "/home/user/985211helper-liaoning2025/cpp/src/index.cpp" "CMakeFiles/helper.dir/src/index.cpp.o" "gcc" "CMakeFiles/helper.dir/src/index.cpp.o.d"
+  "/home/user/985211helper-liaoning2025/cpp/src/json.cpp" "CMakeFiles/helper.dir/src/json.cpp.o" "gcc" "CMakeFiles/helper.dir/src/json.cpp.o.d"
+  "/home/user/985211helper-liaoning2025/cpp/src/search.cpp" "CMakeFiles/helper.dir/src/search.cpp.o" "gcc" "CMakeFiles/helper.dir/src/search.cpp.o.d"
+  "/home/user/985211helper-liaoning2025/cpp/src/signal.cpp" "CMakeFiles/helper.dir/src/signal.cpp.o" "gcc" "CMakeFiles/helper.dir/src/signal.cpp.o.d"
+  "/home/user/985211helper-liaoning2025/cpp/src/usage.cpp" "CMakeFiles/helper.dir/src/usage.cpp.o" "gcc" "CMakeFiles/helper.dir/src/usage.cpp.o.d"
+  "/home/user/985211helper-liaoning2025/cpp/src/version.cpp" "CMakeFiles/helper.dir/src/version.cpp.o" "gcc" "CMakeFiles/helper.dir/src/version.cpp.o.d"
+  "/home/user/985211helper-liaoning2025/cpp/src/web/web.cpp" "CMakeFiles/helper.dir/src/web/web.cpp.o" "gcc" "CMakeFiles/helper.dir/src/web/web.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ example/out/CMakeFiles/helper.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helper.dir/src/data.cpp.o"
+  "CMakeFiles/helper.dir/src/data.cpp.o.d"
+  "CMakeFiles/helper.dir/src/helper.cpp.o"
+  "CMakeFiles/helper.dir/src/helper.cpp.o.d"
+  "CMakeFiles/helper.dir/src/index.cpp.o"
+  "CMakeFiles/helper.dir/src/index.cpp.o.d"
+  "CMakeFiles/helper.dir/src/json.cpp.o"
+  "CMakeFiles/helper.dir/src/json.cpp.o.d"
+  "CMakeFiles/helper.dir/src/search.cpp.o"
+  "CMakeFiles/helper.dir/src/search.cpp.o.d"
+  "CMakeFiles/helper.dir/src/signal.cpp.o"
+  "CMakeFiles/helper.dir/src/signal.cpp.o.d"
+  "CMakeFiles/helper.dir/src/usage.cpp.o"
+  "CMakeFiles/helper.dir/src/usage.cpp.o.d"
+  "CMakeFiles/helper.dir/src/version.cpp.o"
+  "CMakeFiles/helper.dir/src/version.cpp.o.d"
+  "CMakeFiles/helper.dir/src/web/web.cpp.o"
+  "CMakeFiles/helper.dir/src/web/web.cpp.o.d"
+  "helper"
+  "helper.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helper.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
