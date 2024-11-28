a186912e492a5166381bcdb306d4d68ce6347ccc
dpkg123 <test1@qw5d.onmicrosoft.com>
2024-11-28 11:04:28 +0800

    2024年 11月 28日 星期四 11:04:27 CST

--- example/3/RunCMake/ConfigDir/check-reply.cmake
+++
@@ -1,3 +0,0 @@
-if (NOT EXISTS ${RunCMake_TEST_BINARY_DIR}/.cmake/api/v1/reply)
-  set(RunCMake_TEST_FAILED "Failed to read FileAPI query from user config directory")
-endif()
--- example/3/RunCMake/file-MAKE_DIRECTORY/RunCMakeTest.cmake
+++
@@ -1,7 +0,0 @@
-include(RunCMake)
-
-run_cmake_script(MAKE_DIRECTORY-one-dir-FAIL)
-run_cmake_script(MAKE_DIRECTORY-Result-one-dir-FAIL)
-run_cmake_script(MAKE_DIRECTORY-Result-one-dir-SUCCESS)
-run_cmake_script(MAKE_DIRECTORY-Result-many-dirs-FAIL)
-run_cmake_script(MAKE_DIRECTORY-Result-many-dirs-SUCCESS)
