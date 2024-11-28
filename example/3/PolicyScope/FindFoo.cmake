02dad7018a0eec92208bd925112883fab4aceece
dpkg123 <test1@qw5d.onmicrosoft.com>
2024-11-28 10:59:11 +0800

    2024年 11月 28日 星期四 10:58:57 CST

---
+++ example/3/PolicyScope/FindFoo.cmake
@@ -0,0 +1,2 @@
+cmake_minimum_required(VERSION 3.31)
+cmake_policy(SET CMP0180 OLD)
--- example/3/cpp17.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    if constexpr (true) {
-        std::cout << "C++17 is supported.\n";
-    }
-    return 0;
-}
-
